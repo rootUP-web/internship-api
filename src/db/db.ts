4cfdc203de7c5a383e7d8455771c8026a0a9eb7f
User <biswassulekha4@gmail.com>
2025-12-29 13:49:31 +0530

    Code Clean-Up

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,19 +3,7 @@ import { Pool } from 'pg';
 const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
-    // connectionString: connectionString,
-    host: "localhost",
-    user: "apiapp",
-    password: "new_password",
-    database: "apidb",
-    port: 5432,
-});
-
-// const connectionString = 'postgresql://apiapp:new_password@localhost:5432/apidb'
- 
-
-// const pool = new Pool({
-//     connectionString
-// })
+    connectionString
+})
 
 export default pool;
